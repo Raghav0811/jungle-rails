b2ee88b620a2f5cc8fcd77e11b23ca207a7d2921
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2020-07-13 09:32:56 -0400

    routes: admin category feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
+
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  
   resource :about, only: [:show]
-
+  
+  
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
@@ -17,6 +19,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
